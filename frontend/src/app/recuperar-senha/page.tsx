e6879460beda6474f4b9ce441f09a8cefbc463f3
Luis Felipe Pizzi Lopes <luisfelipe1203lf@gmail.com>
2025-03-22 12:34:05 -0300

    feat: reset password screen

--- frontend/src/app/recuperar-senha/page.tsx
+++ frontend/src/app/recuperar-senha/page.tsx
@@ -1,17 +1,16 @@
 "use client";
-
-import { LogIn } from 'lucide-react';
+import { Mail } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
-export default function Login() {
-    const router = useRouter();
+export default function RecuperarSenha() {
+    const router = useRouter(); 
 
     return (
         <div className="flex items-center justify-center min-h-screen bg-base-200">
             <div className="card w-96 bg-base-100 shadow-xl p-6">
-                <h2 className="text-2xl font-bold text-center text-primary">AgroFile</h2>
+                <h2 className="text-2xl font-bold text-center text-primary">Recuperar Senha</h2>
                 <p className="text-center text-sm text-gray-500 mt-2">
-                    Informe suas credenciais para realizar a autenticação
+                    Digite seu e-mail para receber um link de redefinição de senha.
                 </p>
                 <form className="mt-4 space-y-4">
                     <div className="form-control">
@@ -24,21 +23,11 @@ export default function Login() {
                             required
                         />
                     </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text mb-2">Senha</span>
-                        </label>
-                        <input
-                            type="password"
-                            className="input input-bordered w-full"
-                            required
-                        />
-                    </div>
                     <button type="submit" className="btn btn-primary w-full">
-                        <LogIn />Entrar
+                        <Mail /> Enviar Link
                     </button>
                     <div className="text-end">
-                        <a className="link link-primary" onClick={() => router.push("/recuperar-senha")}>Esqueceu sua senha? Clique aqui</a>
+                        <a onClick={()=> router.push("/login")} className="link link-primary">Voltar para login</a>
                     </div>
                 </form>
             </div>
